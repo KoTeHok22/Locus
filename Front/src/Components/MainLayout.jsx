8c9f58d04cdcbebd8cc2e923dfb614fa0cd8547b
KoTeHok22 <kotenok210903@gmail.com>
2025-10-02 14:28:45 +0500

    awd

--- Front/src/Components/MainLayout.jsx
+++ Front/src/Components/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import AuthService from '../AuthService.js';
+import AuthService from "../authService.js";
 import logo from '../logo/logo-mini.ico';
 import { translate } from '../utils/translation.js';
 
