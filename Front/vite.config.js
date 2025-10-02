78d7f78a6fb0d4140891bcef0143708a81357eca
KoTeHok22 <kotenok210903@gmail.com>
2025-10-02 14:26:20 +0500

    server

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['shutdown-team.ru'],
+    host: true,
+  },
   css: {
     transformer: 'postcss'
   }
